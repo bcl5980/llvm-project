8556a41a4ad4e8cf48bc316c9b5692b0de8e3d39
chenglin.bi <chenglin.bi@linaro.org>
2023-02-26 12:49:04 +0800

    create LogicCombiner for every instruction

--- llvm/lib/Transforms/AggressiveInstCombine/AggressiveInstCombine.cpp
+++ llvm/lib/Transforms/AggressiveInstCombine/AggressiveInstCombine.cpp
@@ -838,9 +838,9 @@ static bool foldBitwiseLogic(Function &F, DominatorTree &DT) {
     // finding a solution. We could split the combiner based on types to make
     // the code more efficient, adjust the value of max depth/values, or use
     // APInt to support tracking more than 63 leaf values.
-    LogicCombiner LC;
     for (Instruction &I : BB) {
       if (I.isBitwiseLogicOp()) {
+        LogicCombiner LC;
         Value *NewV = LC.simplify(&I);
         if (NewV) {
           MadeChange = true;
